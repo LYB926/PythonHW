3dd6c61623f17b0e4e83d6031dc0d161585bd0f2
PNRetr0 <zk1019@outlook.com>
2020-03-17 17:16:29 +0800

    Add files via upload
    增加注释。

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
-from get_data import get_data
-from draw import draw
-from output import output
+import tkinter as tk                #使用tkinter生成GUI
+from get_data import get_data       #从网络获取数据的模块
+from draw_line import draw_line     #绘制折线图模块
+from draw_bar import draw_bar       #绘制条形图模块
+from output import output           #进行数据分析并且显示结果模块
 
 win=tk.Tk()
 win.title("StatTrak丨数聚")
-win.geometry('300x350')
+win.geometry('300x430')
 
 l1 = tk.Label(win, text="2月1日-29日全国新冠疫情", fg='black', font=('Arial', 12), width=50, height=1)
 l1.pack()
@@ -18,13 +19,14 @@ l3.pack()
 
 button = tk.Button(win,text="从Internet获取数据",height = 4,width = 20,command=get_data)
 button.pack()
-
-button = tk.Button(win,text="绘制折线图",height = 4,width = 20,command=draw)
+button = tk.Button(win,text="绘制折线图",height = 4,width = 20,command=draw_line)
 button.pack()
-
+button = tk.Button(win,text="绘制条形图",height = 4,width = 20,command=draw_bar)
+button.pack()
 button = tk.Button(win,text="显示数据分析结果",height = 4,width = 20,command=output)
 button.pack()
 
 l3 = tk.Label(win, text="Code by 张可 丨 2020/3/15", fg='black', font=('Arial', 8), width=50, height=2)
 l3.pack()
+
 win.mainloop()
