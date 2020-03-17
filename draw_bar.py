9afaff5729715f4305716a846c7e6ad7b6f6f083
PNRetr0 <zk1019@outlook.com>
2020-03-17 17:15:45 +0800

    Add files via upload
    增加异常处理机制。增加绘制条形图模块。

---
+++ draw_bar.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import csv
+from matplotlib import pyplot as plt
+import tkinter as tk
+import tkinter.messagebox
+
+def draw_bar():
+    date = []
+    diagnosed = []
+    for i in range(1,30):
+        date.append(i)
+    try:
+        with open ("data_csv.csv") as csvfile:
+            plots = csv.reader(csvfile,delimiter = ",")
+            for row in plots:
+                diagnosed.append(int(row[0]))
+        plt.bar(date,diagnosed,align="center")
+        plt.axis([0,30,0,15000])
+        plt.xlabel("Date")
+        plt.ylabel("New confirmed patients")
+        plt.title("New daily diagnoses in China (February 1 to February 29)")
+        plt.show()
+    except FileNotFoundError:
+        tk.messagebox.showerror(title='Error!', message='您还未获取数据，请先获取数据。')
