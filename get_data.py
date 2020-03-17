1145c141a6bea1623dcde9707ffa35b6c25f2283
PNRetr0 <zk1019@outlook.com>
2020-03-17 17:38:19 +0800

    Add files via upload
    增加异常处理机制。

--- get_data.py
+++ get_data.py
@@ -2,12 +2,17 @@
 
 import requests
 import csv
+import os
 import tkinter as tk
 import tkinter.messagebox
 
 def get_data():
     url = 'https://gitee.com/PNRetr0/PythonHW/raw/master/data_csv.CSV'
-    data = requests.get(url)
-    with open("data_csv.csv","wb") as data_file:
-        data_file.write(data.content)
-    tk.messagebox.showinfo(title='All done!', message='数据获取完成，请进行可视化和数据分析。')
+    try:
+        data = requests.get(url)
+        with open("data_csv.csv","wb") as data_file:
+            data_file.write(data.content)
+        tk.messagebox.showinfo(title='All done!', message='数据获取完成，请进行可视化和数据分析。')
+    except Exception:
+        tk.messagebox.showerror(title='Error!', message='计算机似乎未连接到Internet。将运行ping指令。')
+        os.system(u"ping 114.114.114.114")
